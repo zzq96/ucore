358afbf752f4e29b0a540cc771ec1a991c86657f
zzq96 <1402806685@qq.com>
2021-05-27 18:46:24 +0800

    init lab3

--- labcodes/lab3/kern/mm/pmm.h
+++ labcodes/lab3/kern/mm/pmm.h
@@ -10,16 +10,17 @@
 // pmm_manager is a physical memory management class. A special pmm manager - XXX_pmm_manager
 // only needs to implement the methods in pmm_manager class, then XXX_pmm_manager can be used
 // by ucore to manage the total physical memory space.
-struct pmm_manager {
+struct pmm_manager
+{
     const char *name;                                 // XXX_pmm_manager's name
     void (*init)(void);                               // initialize internal description&management data structure
-                                                      // (free block list, number of free block) of XXX_pmm_manager 
+                                                      // (free block list, number of free block) of XXX_pmm_manager
     void (*init_memmap)(struct Page *base, size_t n); // setup description&management data structcure according to
-                                                      // the initial free physical memory space 
-    struct Page *(*alloc_pages)(size_t n);            // allocate >=n pages, depend on the allocation algorithm 
+                                                      // the initial free physical memory space
+    struct Page *(*alloc_pages)(size_t n);            // allocate >=n pages, depend on the allocation algorithm
     void (*free_pages)(struct Page *base, size_t n);  // free >=n pages with "base" addr of Page descriptor structures(memlayout.h)
-    size_t (*nr_free_pages)(void);                    // return the number of free pages 
-    void (*check)(void);                              // check the correctness of XXX_pmm_manager 
+    size_t (*nr_free_pages)(void);                    // return the number of free pages
+    void (*check)(void);                              // check the correctness of XXX_pmm_manager
 };
 
 extern const struct pmm_manager *pmm_manager;
@@ -51,96 +52,110 @@ void print_pgdir(void);
  * where the machine's maximum 256MB of physical memory is mapped and returns the
  * corresponding physical address.  It panics if you pass it a non-kernel virtual address.
  * */
-#define PADDR(kva) ({                                                   \
-            uintptr_t __m_kva = (uintptr_t)(kva);                       \
-            if (__m_kva < KERNBASE) {                                   \
-                panic("PADDR called with invalid kva %08lx", __m_kva);  \
-            }                                                           \
-            __m_kva - KERNBASE;                                         \
-        })
+#define PADDR(kva) ({                                          \
+    uintptr_t __m_kva = (uintptr_t)(kva);                      \
+    if (__m_kva < KERNBASE)                                    \
+    {                                                          \
+        panic("PADDR called with invalid kva %08lx", __m_kva); \
+    }                                                          \
+    __m_kva - KERNBASE;                                        \
+})
 
 /* *
  * KADDR - takes a physical address and returns the corresponding kernel virtual
  * address. It panics if you pass an invalid physical address.
  * */
-#define KADDR(pa) ({                                                    \
-            uintptr_t __m_pa = (pa);                                    \
-            size_t __m_ppn = PPN(__m_pa);                               \
-            if (__m_ppn >= npage) {                                     \
-                panic("KADDR called with invalid pa %08lx", __m_pa);    \
-            }                                                           \
-            (void *) (__m_pa + KERNBASE);                               \
-        })
+#define KADDR(pa) ({                                         \
+    uintptr_t __m_pa = (pa);                                 \
+    size_t __m_ppn = PPN(__m_pa);                            \
+    if (__m_ppn >= npage)                                    \
+    {                                                        \
+        panic("KADDR called with invalid pa %08lx", __m_pa); \
+    }                                                        \
+    (void *)(__m_pa + KERNBASE);                             \
+})
 
 extern struct Page *pages;
 extern size_t npage;
 
 static inline ppn_t
-page2ppn(struct Page *page) {
+page2ppn(struct Page *page)
+{
     return page - pages;
 }
 
 static inline uintptr_t
-page2pa(struct Page *page) {
+page2pa(struct Page *page)
+{
     return page2ppn(page) << PGSHIFT;
 }
 
 static inline struct Page *
-pa2page(uintptr_t pa) {
-    if (PPN(pa) >= npage) {
+pa2page(uintptr_t pa)
+{
+    if (PPN(pa) >= npage)
+    {
         panic("pa2page called with invalid pa");
     }
     return &pages[PPN(pa)];
 }
 
 static inline void *
-page2kva(struct Page *page) {
+page2kva(struct Page *page)
+{
     return KADDR(page2pa(page));
 }
 
 static inline struct Page *
-kva2page(void *kva) {
+kva2page(void *kva)
+{
     return pa2page(PADDR(kva));
 }
 
 static inline struct Page *
-pte2page(pte_t pte) {
-    if (!(pte & PTE_P)) {
+pte2page(pte_t pte)
+{
+    if (!(pte & PTE_P))
+    {
         panic("pte2page called with invalid pte");
     }
     return pa2page(PTE_ADDR(pte));
 }
 
 static inline struct Page *
-pde2page(pde_t pde) {
+pde2page(pde_t pde)
+{
     return pa2page(PDE_ADDR(pde));
 }
 
 static inline int
-page_ref(struct Page *page) {
+page_ref(struct Page *page)
+{
     return page->ref;
 }
 
 static inline void
-set_page_ref(struct Page *page, int val) {
+set_page_ref(struct Page *page, int val)
+{
     page->ref = val;
 }
 
 static inline int
-page_ref_inc(struct Page *page) {
+page_ref_inc(struct Page *page)
+{
     page->ref += 1;
     return page->ref;
 }
 
 static inline int
-page_ref_dec(struct Page *page) {
+page_ref_dec(struct Page *page)
+{
     page->ref -= 1;
     return page->ref;
 }
 
 extern char bootstack[], bootstacktop[];
 
-extern void * kmalloc(size_t n);
+extern void *kmalloc(size_t n);
 extern void kfree(void *ptr, size_t n);
 #endif /* !__KERN_MM_PMM_H__ */
-
