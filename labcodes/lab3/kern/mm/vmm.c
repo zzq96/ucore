acafc28f09bf9137af4fa5df1068d14222d0f81e
zzq96 <1402806685@qq.com>
2021-05-28 14:33:26 +0800

    finished lab3 1

--- labcodes/lab3/kern/mm/vmm.c
+++ labcodes/lab3/kern/mm/vmm.c
@@ -52,7 +52,7 @@ mm_create(void)
         mm->map_count = 0;
 
         if (swap_init_ok)
-            swap_init_mm(mm);
+            swap_init_mm(mm); //将sm_prev（swap manager）赋值为一个le， 他是FIFO维护链表的头结点的le
         else
             mm->sm_priv = NULL;
     }
@@ -353,8 +353,8 @@ int do_pgfault(struct mm_struct *mm, uint32_t error_code, uintptr_t addr)
             goto failed;
         }
         break;
-    case 1: /* error code flag : (W/R=0, P=1): read, present */
-        cprintf("do_pgfault failed: error code flag = read AND present\n");
+    case 1:                                                                 /* error code flag : (W/R=0, P=1): read, present */
+        cprintf("do_pgfault failed: error code flag = read AND present\n"); //啥玩意，这是什么错误
         goto failed;
     case 0: /* error code flag : (W/R=0, P=0): read, not present */
         if (!(vma->vm_flags & (VM_READ | VM_EXEC)))
@@ -396,15 +396,27 @@ int do_pgfault(struct mm_struct *mm, uint32_t error_code, uintptr_t addr)
     *   mm->pgdir : the PDT of these vma
     *
     */
-#if 0
+#if 1
     /*LAB3 EXERCISE 1: YOUR CODE*/
-    ptep = ???              //(1) try to find a pte, if pte's PT(Page Table) isn't existed, then create a PT.
-    if (*ptep == 0) {
-                            //(2) if the phy addr isn't exist, then alloc a page & map the phy addr with logical addr
-
+    ptep = get_pte(mm->pgdir, addr, 1); //(1) try to find a pte, if pte's PT(Page Table) isn't existed, then create a PT.
+    if (ptep == NULL)
+    {
+        cprintf("get pte in do_pgfault failed!");
+        goto failed;
     }
-    else {
-    /*LAB3 EXERCISE 2: YOUR CODE
+    if (*ptep == 0)
+    {
+        //(2) if the phy addr isn't exist, then alloc a page & map the phy addr with logical addr
+        struct Page *page = pgdir_alloc_page(mm->pgdir, addr, perm);
+        if (page == NULL)
+        {
+            cprintf("alloc page failed on do_pgfault!\n");
+            goto failed;
+        }
+    }
+    else
+    {
+        /*LAB3 EXERCISE 2: YOUR CODE
     * Now we think this pte is a  swap entry, we should load data from disk to a page with phy addr,
     * and map the phy addr with logical addr, trigger swap manager to record the access situation of this page.
     *
@@ -415,18 +427,20 @@ int do_pgfault(struct mm_struct *mm, uint32_t error_code, uintptr_t addr)
     *    page_insert ： build the map of phy addr of an Page with the linear addr la
     *    swap_map_swappable ： set the page swappable
     */
-        if(swap_init_ok) {
-            struct Page *page=NULL;
-                                    //(1）According to the mm AND addr, try to load the content of right disk page
-                                    //    into the memory which page managed.
-                                    //(2) According to the mm, addr AND page, setup the map of phy addr <---> logical addr
-                                    //(3) make the page swappable.
+        if (swap_init_ok)
+        {
+            struct Page *page = NULL;
+            //(1）According to the mm AND addr, try to load the content of right disk page
+            //    into the memory which page managed.
+            //(2) According to the mm, addr AND page, setup the map of phy addr <---> logical addr
+            //(3) make the page swappable.
         }
-        else {
-            cprintf("no swap_init_ok but ptep is %x, failed\n",*ptep);
+        else
+        {
+            cprintf("no swap_init_ok but ptep is %x, failed\n", *ptep);
             goto failed;
         }
-   }
+    }
 #endif
     ret = 0;
 failed:
